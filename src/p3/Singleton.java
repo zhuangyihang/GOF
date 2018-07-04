e5bcf8b899c4defc621109eb5c5d1019417bb1e5
zyh <zhuangyihang@pugongying.com.cn>
2018-07-04 21:04:06 +0800

    设计模式

--- src/p3/Singleton.java
+++ src/p3/Singleton.java
@@ -13,9 +13,7 @@ public class Singleton {
   private static Singleton singleton = new Singleton();
 
   public static Singleton getInstance() {
-    if (singleton == null) {
-      singleton = new Singleton();
-    }
+
     return singleton;
   }
 
